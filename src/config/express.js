d8c62761496d0fc49b4c28dbe1fc9a27c20a3a12
foxy1482 <tobiaslabo@gmail.com>
2025-03-05 17:42:44 -0300

    Http and Mongo configured

---
+++ src/config/express.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+const expressApp = express();
+
+expressApp.listen() 
+// TODO: Añadir los middlewares, routes...
+
+export default expressApp;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-import { printHello } from '#Lib/hello.js';
+import httpServer from "#Config/http.js";
+import '#Config/env.js';
+import connectDB from "#Config/db.js";
 
-printHello();
+const bootstrap = async ()=>{
+    await connectDB(process.env.MONGODB_URL);
+    httpServer.listen(process.env.PORT, ()=>{
+        console.log(`Servidor a la escucha en el puerto ${process.env.PORT}`);
+    });
+};
+bootstrap();
